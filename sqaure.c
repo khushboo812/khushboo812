3c056494fb3e86f518fb008dcbf5ef4d99ae3b8e
khushboo812 <khushbootamboli641@gmail.com>
2025-08-17 13:32:50 +0530

    Add files via upload

---
+++ sqaure.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int side, area;;
+    printf("enter the side of the sqaure");
+    scanf("%d",&side);
+    area = side*side;
+    printf("area of the sqaure is %d", area);
+    return 0;
+}
